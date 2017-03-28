aff0e6a25eb70e41bf3a5a51647ce28955d5b058
paramansh <paramansh9895@gmail.com>
2017-03-28 18:21:17 +0000

    broadcast

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var url = require('url');
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -12,14 +13,18 @@ app.get('/usr2', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket){
   console.log('a user connected');
   //socket.emit
+  //This socket.on event is after the socket.emit event of the browser
   socket.on('chat messages', function(msg){
-    socket.broadcast.emit('chat messages', msg);//To emit messages to only other persons of the group
-    
+    socket.emit('chat message', 'me: ' + msg);//message received by the user
+    /**
+    *This socket.emit is the socket.on event received by the user on the client side
+    */
+    socket.broadcast.emit('chat message', msg);//To emit messages to only other persons of the group
     //socket.broadcast.emit('hi');
-
   });
 });
 
